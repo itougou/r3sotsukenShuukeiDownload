486ed620531758532b1e8fca4eb5f3b9823ce94f
gogo <gogo@192.168.11.102>
2021-12-28 21:39:11 +0900

    コメント修正

--- demo2/src/main/java/com/example/demo/mapper/AttendanceMapper.java
+++ demo2/src/main/java/com/example/demo/mapper/AttendanceMapper.java
@@ -9,11 +9,11 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface AttendanceMapper {
 
-	//科目名取り出し
+	//科目名取り出しSQL実行メソッド
 	//	[科目名、科目名、科目名、・・・・・、科目名]
 	List<String> getAttendanceKamoku();
 
-	// 学生・科目毎出席数集計SQL （ HashMap(1行分のﾃﾞｰﾀ)をListにして取得 ）
+	// 学生・科目毎出席数集計 （ HashMap(1行分のﾃﾞｰﾀ)をListにして取得 ）SQL実行メソッド
 	//	[ キー：値, キー：値, ・・・・・, キー：値　]
 	//	[ キー：値, キー：値, ・・・・・, キー：値　]
 	// 			：
